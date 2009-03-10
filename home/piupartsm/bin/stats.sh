0ba986ad5ff293d0cb0cff67047af8cd4d60e9f1
Holger Levsen (holger) <holger@debian.org>
2009-03-10 21:40:44 +0000

    make relative stats

--- home/piupartsm/bin/stats.sh
+++ home/piupartsm/bin/stats.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-cd ~piupartsm/piuparts-master/
+#cd ~piupartsm/piuparts-master/
 
 set -e
 
@@ -9,7 +9,5 @@ do
     echo $(date "+%Y-%m-%d %H:%M") \
         pass: $(ls pass | wc -l) \
         fail: $(ls fail | wc -l) \
-	spass: $(ls ~piupartss/pass | wc -l) \
-	sfail: $(ls ~piupartss/fail | wc -l)
     sleep 600
 done
