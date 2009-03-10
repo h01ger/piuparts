c5128fd54de1a67f298842cc72fdcf762ffb5a83
Holger Levsen (holger) <holger@debian.org>
2009-03-10 21:43:58 +0000

    fix syntax error

--- home/piupartsm/bin/stats.sh
+++ home/piupartsm/bin/stats.sh
@@ -8,6 +8,6 @@ while true
 do
     echo $(date "+%Y-%m-%d %H:%M") \
         pass: $(ls pass | wc -l) \
-        fail: $(ls fail | wc -l) \
+        fail: $(ls fail | wc -l) ; \
     sleep 600
 done
