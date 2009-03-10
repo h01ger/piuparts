f3f68b2099d0d5da1828a6f8391894b15ce1fb1d
Holger Levsen (holger) <holger@debian.org>
2009-03-10 15:49:20 +0000

    some debugging and apt-updates here and there..

--- org/piuparts.debian.org/etc/scripts/pre_distupgrade_apt.sh
+++ org/piuparts.debian.org/etc/scripts/pre_distupgrade_apt.sh
@@ -1,4 +1,7 @@
-#!/bin/bash
+#!/bin/sh
 
+apt-get update
+echo debugXXX
+cat /etc/apt/sources.list
 apt-get -yf install apt
 apt-get -yf dist-upgrade
--- org/piuparts.debian.org/etc/scripts/pre_distupgrade_tzdata.sh
+++ org/piuparts.debian.org/etc/scripts/pre_distupgrade_tzdata.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
 
+apt-get update 
+echo debug XXX
+cat /etc/apt/sources.list
 dpkg -l tzdata && apt-get install libstdc++6 && apt-get dist-upgrade
