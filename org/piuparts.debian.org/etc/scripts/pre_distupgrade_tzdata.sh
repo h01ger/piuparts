85ac4d2f326d8a4a32cafaf65bacb955906af9d2
Holger Levsen (holger) <holger@debian.org>
2009-03-10 16:02:15 +0000

    use even more force

--- org/piuparts.debian.org/etc/scripts/pre_distupgrade_tzdata.sh
+++ org/piuparts.debian.org/etc/scripts/pre_distupgrade_tzdata.sh
@@ -5,7 +5,7 @@ echo "DebugXXX: running $0"
 apt-get update 
 cat /etc/apt/sources.list
 dpkg -l tzdata && { 
-	apt-get install libstdc++6
+	apt-get -y --force-yes install libstdc++6
 	apt-get upgrade
 	apt-get dist-upgrade
 }
