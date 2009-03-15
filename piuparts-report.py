27c5b0bbd1deea32f65ee0118c36930f666f7e55
Holger Levsen (holger) <holger@debian.org>
2009-03-15 18:10:20 +0000

    gotta has stylez

--- piuparts-report.py
+++ piuparts-report.py
@@ -42,7 +42,7 @@ HTML_HEADER = """
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
  <html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
   <title>piatti.debian.org / piatti.cs.helsinki.fi</title>
-  <link type="text/css" rel="stylesheet" href="style.css">
+  <link type="text/css" rel="stylesheet" href="/style.css">
   <link rel="shortcut icon" href="/favicon.ico">
  </head>
  <body>
