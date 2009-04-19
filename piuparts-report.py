7cf37b35416cd650851a7d5f6e67e7ea2a1a5c8a
Holger Levsen (holger) <holger@debian.org>
2009-04-19 23:39:21 +0000

    dependency-failed-testing is also a reason to emphasize

--- piuparts-report.py
+++ piuparts-report.py
@@ -439,7 +439,7 @@ def html_protect(str):
 
 
 def emphasize_reason(str):
-    if str == "unknown-package" or str == "failed-testing" or str == "circular-dependency":
+    if str == "unknown-package" or str == "failed-testing" or str == "circular-dependency" or str == "dependency-failed-testing":
       str = "<em>" + str + "</em>"
     return str
 
