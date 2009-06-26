50a20abe4cd1ab7ceac2fbcfe76b7b71489711bf
Holger Levsen (holger) <holger@debian.org>
2009-06-26 10:12:15 +0000

    revert liw´s change as it doesnt work. even with s#seq#s#

--- piupartslib/packagesdb.py
+++ piupartslib/packagesdb.py
@@ -47,7 +47,42 @@ def rfc822_like_header_parse(input):
     return headers
  
 def unique (s):
-    return list(set(seq))
+    # taken from http://code.activestate.com/recipes/52560/ - thanks to Tim Peters
+    n = len(s)
+    if n == 0:
+      return []  
+
+    u = {}
+    try:
+      for x in s:
+          u[x] = 1
+    except TypeError:
+      del u  # move on to the next method
+    else:
+      return u.keys()   
+
+    try:
+      t = list(s)
+      t.sort()
+    except TypeError:
+      del t  # move on to the next method
+    else:
+      assert n > 0
+      last = t[0]
+      lasti = i = 1
+      while i < n:
+          if t[i] != last:
+              t[lasti] = last = t[i]
+              lasti += 1
+          i += 1
+      return t[:lasti]
+
+    # Brute force is all that's left.
+    u = []
+    for x in s:
+      if x not in u:
+          u.append(x)
+    return u
 
 class Package(UserDict.UserDict):
 
