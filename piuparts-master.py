c4e4fd43c77d1c354dd518ba3b3d45fa1812953e
Holger Levsen (holger) <holger@debian.org>
2010-11-06 22:40:20 +0000

    closer

--- piuparts-master.py
+++ piuparts-master.py
@@ -56,6 +56,7 @@ class Config(piupartslib.conf.Config):
                 "log-file": None,
                 "packages-url": None,
                 "master-directory": ".",
+                 "known_circular_depends": [],
             }, "")
 
 
