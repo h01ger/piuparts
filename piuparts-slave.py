e22bd63a3d1e77dd34ef30cb23bf1496cad3fb7c
Holger Levsen <holger@layer-acht.org>
2012-01-20 00:26:03 +0100

    whitespace only

--- piuparts-slave.py
+++ piuparts-slave.py
@@ -193,7 +193,7 @@ class Slave:
         line = self._readline()
         words = line.split()
         if words and words[0] == "ok":
-            logging.info("Master " + section + "status: " + " ".join(words[1:]))
+            logging.info("Master " + section + " status: " + " ".join(words[1:]))
         else:
             raise MasterIsCrazy()
 
