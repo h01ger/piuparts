f753a45fdabeee6a60e1e57a4d3aa756bc04096f
Andreas Beckmann <debian@abeckmann.de>
2012-11-02 21:12:18 +0100

    p: add some more checks for --install-purge-install
    Signed-off-by: Andreas Beckmann <debian@abeckmann.de>

--- piuparts.py
+++ piuparts.py
@@ -2144,6 +2144,9 @@ def install_purge_test(chroot, chroot_state, package_files, packages):
         file_owners = chroot.get_files_owned_by_packages()
         chroot.restore_selections(chroot_state_with_deps["selections"], packages)
         logging.info("Validating chroot after purge")
+        chroot.check_debsums()
+        chroot.check_for_no_processes()
+        chroot.check_for_broken_symlinks()
         if not check_results(chroot, chroot_state_with_deps, file_owners, deps_info=deps_info):
             return False
         logging.info("Reinstalling after purge")
