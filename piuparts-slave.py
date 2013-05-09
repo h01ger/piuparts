51794c9f484eef221538539a8f61dda35d3bf62c
Andreas Beckmann <anbe@debian.org>
2013-05-10 01:56:59 +0200

    p-s: include arch in the default tarball name
    allows running both amd64 and i386 on the same slave with minimal configuration
    
    Signed-off-by: Andreas Beckmann <anbe@debian.org>

--- piuparts-slave.py
+++ piuparts-slave.py
@@ -401,7 +401,8 @@ class Section:
 
     def _get_tarball(self):
         basetgz = self._config["chroot-tgz"] or \
-                self._distro_config.get_basetgz(self._config.get_start_distro())
+                self._distro_config.get_basetgz(self._config.get_start_distro(),
+                                                self._config.get_arch())
         return os.path.join(self._config["basetgz-directory"], basetgz)
 
     def _check_tarball(self):
