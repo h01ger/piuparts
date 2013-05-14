27c18b44c5d24966351a68d57d94393e03ac6a4c
Andreas Beckmann <anbe@debian.org>
2013-05-14 20:04:57 +0200

    p: really disable using pdiffs
    pdiffs cost a lot of cpu in order to save some bandwidth
    that's useless with a local mirror or proxy
    
    Signed-off-by: Andreas Beckmann <anbe@debian.org>

--- piuparts.py
+++ piuparts.py
@@ -961,7 +961,7 @@ class Chroot:
         lines.append('APT::Install-Recommends "%d";\n' % int(settings.install_recommends))
         lines.append('APT::Install-Suggests "0";\n')
         lines.append('APT::Get::AllowUnauthenticated "%s";\n' % settings.apt_unauthenticated)
-        lines.append('APT::Acquire::PDiffs "false";\n')
+        lines.append('Acquire::PDiffs "false";\n')
         if settings.proxy:
             proxy = settings.proxy
         elif "http_proxy" in os.environ:
