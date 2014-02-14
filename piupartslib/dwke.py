288cc1b35ae5ff6f931c535651ce533474fbca93
Holger Levsen <holger@layer-acht.org>
2014-02-14 14:01:02 +0100

    Nothing really yet. Added a comment...

--- piupartslib/dwke.py
+++ piupartslib/dwke.py
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU General Public License along
 # with this program; if not, see <http://www.gnu.org/licenses/>.
 
+# dwke.py is used by master-bin/detect_well_known_errors.py
 
 import os
 import logging
