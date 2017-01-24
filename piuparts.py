1839d44573987589e8634031cb1f996049556841
Holger Levsen <holger@layer-acht.org>
2017-01-24 11:39:34 +0100

    Revert "disable apt installing .deb files while investigating problems"
    This reverts commit 6ac6ab90dde2799003340162581e5efabc447b36.

--- piuparts.py
+++ piuparts.py
@@ -1204,7 +1204,6 @@ class Chroot:
             #   dummy package
             (status, output) = self.run(["dpkg-query", "-f", "${Version}\n", "-W", "apt"], ignore_errors=True)
             apt_can_install_debs = LooseVersion(output.strip()) >= LooseVersion("1.1")
-            apt_can_install_debs = False
 
             if with_scripts:
                 self.run_scripts("pre_install")
