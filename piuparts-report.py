c099c1f47015e839bc216fb72d00d24693adad28
Andreas Beckmann <anbe@debian.org>
2017-06-18 15:09:27 +0200

    known problems: detect trigger problems
    Signed-off-by: Andreas Beckmann <anbe@debian.org>
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- piuparts-report.py
+++ piuparts-report.py
@@ -474,6 +474,7 @@ linktarget_by_template = [
     ("db_setup_error.tpl", "due to failing to setup a database"),
     ("insserv_error.tpl", "due to a problem with insserv"),
     ("problems_and_no_force_error.tpl", "due to not enough force being used"),
+    ("trigger_cycle_error.tpl", "due to dpkg encountered trigger problems"),
     ("immediate_configuration_error.tpl", "due to apt could not perform immediate configuration"),
     ("pre_depends_error.tpl", "due to a problem with pre-depends"),
     ("pre_installation_script_error.tpl", "due to pre-installation maintainer script failed"),
