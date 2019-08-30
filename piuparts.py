1da95bc5115bfe99944883630a5833cbc6d40679
Holger Levsen <holger@layer-acht.org>
2019-08-30 15:04:41 +0200

    update examples for buster being stable and bullseye testing
    Signed-off-by: Holger Levsen <holger@layer-acht.org>

--- piuparts.py
+++ piuparts.py
@@ -2684,7 +2684,7 @@ def parse_command_line():
 
     parser.add_option("-d", "--distribution", action="append", metavar="NAME",
                       help="Which Debian distribution to use: a code name " +
-                           "(for example jessie, stretch, sid) or experimental. The " +
+                           "(for example buster, bullseye, sid) or experimental. The " +
                            "default is sid (=unstable).")
 
     parser.add_option("-D", "--defaults", action="store",
